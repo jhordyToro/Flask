da942d624ec47fc897c210ee183e500231ea52e3
Jhory Toro <jhordytoro49@gmail.com>
2022-03-31 21:37:13 -0500

    'codigo_documentado'

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from flask import Flask 
+from flask import Flask, redirect #Es un mini framework de Python creador de APIs como FastAPI(todavia no que se diferencia tienen xd)
+from flask import request #nos sirve para preguntar y obtener una respuesta del server con la ip (request Body)
+from flask import make_response
+from flask import render_template # puede renderizar archivos HTML para retornarlos (es obligatorio crear una carpeta con el nombre 'templates' o si no el programa no encuentra el archivo)
 
 app = Flask(__name__)
 
+Todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
 @app.route('/')
+def index():
+    user_ip = request.remote_addr ## aqui estamos soicitando la IP
+    rediret = make_response(redirect('/home')) #estamos instanciando una variale con una respuesta (response) que redirige (redirect) a la route (/home)
+    rediret.set_cookie('user_ip', user_ip) #Con esa misma variable enviamos una cookie con una key(apodo) y un valor 
+
+    return rediret #posteriormente retornamos la misma (si no lo hacemos no nos redirecciona a ningun lado y por lo mismo da error por no retornar ningun valor)
+
+@app.route('/home')
 def home():
-    return '<h1>Hello World Flask</h1>'
+    
+    user_ip = request.cookies.get('user_ip') #Hacemos un requeste preguntandole al server sobre la cookie que tiene nombre 'user_ip' y lo gurdamos en una variable
+    
+    context = {
+        'todos': Todos,
+        'user_ip': user_ip
+    }
+
+    return  render_template('hello.html', **context) #Un templeate -> archivo de HTML -> renderiza informacion: Estatica o DInamica -> por variables -> luego nos muestra en el navegador
+
+if __name__ == '__main__':
+    app.run(debug=True) #esto es para hacer que el codigo corra sin hacer ningun (set FLASK_APP=main o set FLASK_DEBUG=1 y fast run) directamente lo instanciamos en el codigo
