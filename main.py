8142a14439b41bfdeeb771c471f93181567cc573
Jhory Toro <jhordytoro49@gmail.com>
2022-04-02 22:09:28 -0500

    'configure_error_pages'

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ app = Flask(__name__)
 
 Todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
+#CODIGOS HTTP (HTTP status)
+@app.errorhandler(404) ## este decorador sirve para regresar una respuesta en caso de que nos levante el codigo 404
+def not_found(error): ## esta esta acompañada del este decorador asi que preparamos una respuesta en caso de ocurra este error en ves de dejar que el propio navegador responda (no me supe explicar xd)
+    return render_template('404.html', error= error) ## le decimos que archivo mandar cuando este error ocurra en mi caso yo puse un gato :)
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
+
+
+#ROUTES (rutas)
 @app.route('/')
 def index():
     user_ip = request.remote_addr ## aqui estamos soicitando la IP
@@ -26,6 +38,8 @@ def home():
     }
 
     return  render_template('hello.html', **context) #Un templeate -> archivo de HTML -> renderiza informacion: Estatica o DInamica -> por variables -> luego nos muestra en el navegador
+
+
 
 if __name__ == '__main__':
     app.run(debug=True) #esto es para hacer que el codigo corra sin hacer ningun (set FLASK_APP=main o set FLASK_DEBUG=1 y fast run) directamente lo instanciamos en el codigo
