8ea8907d8b7411d017d0905967bad73e7b46b3c7
Jhory Toro <jhordytoro49@gmail.com>
2022-04-04 18:14:32 -0500

    '3'

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from crypt import methods
 from flask import Flask, redirect #Es un mini framework de Python creador de APIs como FastAPI(todavia no que se diferencia tienen xd)
 from flask import request #nos sirve para preguntar y obtener una respuesta del server con la ip (request Body)
-from flask import make_response
+from flask import make_response, url_for
 from flask import render_template # puede renderizar archivos HTML para retornarlos (es obligatorio crear una carpeta con el nombre 'templates' o si no el programa no encuentra el archivo)
 from flask_bootstrap import Bootstrap # es una libreria de Flask que nos permite decorar mas nuestro sitio web con su propia sintaxis
 from flask import session # nos permite guardar informacion que permanece entre cada request (manda la informacion incriptada a diferencia de las cookies)
@@ -34,27 +35,41 @@ def internal_server_error(error):
 
 
 #ROUTES (rutas)
+
+# @app.route('/')
+# def index():
+#     raise(Exception('500 error'))
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr ## aqui estamos soicitando la IP
     rediret = make_response(redirect('/home')) #estamos instanciando una variale con una respuesta (response) que redirige (redirect) a la route (/home)
     # rediret.set_cookie('user_ip', user_ip)  --> #Con esa misma variable enviamos una cookie con una key(apodo) y un valor 
     session['user_ip'] = user_ip # nos permite guardar informacionde manera segura se guarda y se manda 
+    
 
     return rediret #posteriormente retornamos la misma (si no lo hacemos no nos redirecciona a ningun lado y por lo mismo da error por no retornar ningun valor)
 
-@app.route('/home')
+
+
+@app.route('/home', methods=['GET','POST'])
 def home():
     
     # user_ip = request.cookies.get('user_ip') --> #Hacemos un requeste preguntandole al server sobre la cookie que tiene nombre 'user_ip' y lo gurdamos en una variable
     user_ip = session.get('user_ip') # pedimos el valor que se guardo anteriormente y lo guardamos en otra variable (en este caso con el mismo nobre)
     login = login_form()
+    user_name = session.get('username')
 
     context = {
         'todos': Todos,
         'user_ip': user_ip,
-        'login': login
+        'login': login,
+        'username': user_name
     }
+    if login.validate_on_submit():
+        user_name = login.user_name.data
+        session['username'] = user_name
+        return redirect(url_for('index'))
 
     return  render_template('hello.html', **context) #Un templeate -> archivo de HTML -> renderiza informacion: Estatica o DInamica -> por variables -> luego nos muestra en el navegador
 
