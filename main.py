373f244bf9df54a02752bffc7c811754ebd7b2c6
Jhory Toro <jhordytoro49@gmail.com>
2022-04-04 15:30:59 -0500

    'session'

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from flask import Flask, redirect #Es un mini framework de Python creador de API
 from flask import request #nos sirve para preguntar y obtener una respuesta del server con la ip (request Body)
 from flask import make_response
 from flask import render_template # puede renderizar archivos HTML para retornarlos (es obligatorio crear una carpeta con el nombre 'templates' o si no el programa no encuentra el archivo)
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap # es una libreria de Flask que nos permite decorar mas nuestro sitio web con su propia sintaxis
+from flask import session # nos permite guardar informacion que permanece entre cada request (manda la informacion incriptada a diferencia de las cookies)
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap(app) # asi se inicializa BootsTrap
+
+app.config['SECRET_KEY'] = '!·$%&/()' # nos permite configurar una llave secreta 
 
 Todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
@@ -26,15 +29,17 @@ def internal_server_error(error):
 def index():
     user_ip = request.remote_addr ## aqui estamos soicitando la IP
     rediret = make_response(redirect('/home')) #estamos instanciando una variale con una respuesta (response) que redirige (redirect) a la route (/home)
-    rediret.set_cookie('user_ip', user_ip) #Con esa misma variable enviamos una cookie con una key(apodo) y un valor 
+    # rediret.set_cookie('user_ip', user_ip)  --> #Con esa misma variable enviamos una cookie con una key(apodo) y un valor 
+    session['user_ip'] = user_ip # nos permite guardar informacionde manera segura se guarda y se manda 
 
     return rediret #posteriormente retornamos la misma (si no lo hacemos no nos redirecciona a ningun lado y por lo mismo da error por no retornar ningun valor)
 
 @app.route('/home')
 def home():
     
-    user_ip = request.cookies.get('user_ip') #Hacemos un requeste preguntandole al server sobre la cookie que tiene nombre 'user_ip' y lo gurdamos en una variable
-    
+    # user_ip = request.cookies.get('user_ip') --> #Hacemos un requeste preguntandole al server sobre la cookie que tiene nombre 'user_ip' y lo gurdamos en una variable
+    user_ip = session.get('user_ip') # pedimos el valor que se guardo anteriormente y lo guardamos en otra variable (en este caso con el mismo nobre)
+
     context = {
         'todos': Todos,
         'user_ip': user_ip
